2000a8d6c3212d63b9fe7dfcb8e034e77e167349
FDrag0n <34733637+FDrag0n@users.noreply.github.com>
2026-08-19 05:07:48 +0800

    Commit fixed signing keystore for consistent release signatures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,13 +21,6 @@ android {
         if (keystoreFile.exists()) {
             create("release") {
                 storeFile = keystoreFile
-                storePassword = System.getenv("KEYSTORE_PASSWORD")
-                keyAlias = System.getenv("KEY_ALIAS")
-                keyPassword = System.getenv("KEY_PASSWORD")
-            }
-        } else {
-            create("release") {
-                storeFile = file(System.getProperty("user.home") + "/.android/debug.keystore")
                 storePassword = "android"
                 keyAlias = "androiddebugkey"
                 keyPassword = "android"
